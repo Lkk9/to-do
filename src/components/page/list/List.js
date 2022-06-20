d3dcbe891ebb4277be92fdc861b73d4091be54ca
Lkk9 <l.t.446kk@gmail.com>
2022-06-20 16:59:17 +0300

    bug fix

--- src/components/page/list/List.js
+++ src/components/page/list/List.js
@@ -44,7 +44,7 @@ const List = ({isMain, pageKey}) => {
 
   return <div className="List">
     {(()=>{
-      if (!tools.getData(pageKey).list) localStorage.removeItem(pageKey) // :\
+      if (!tools.getData(pageKey).list) localStorage.clear() // :\
       else return [...tools.getData(pageKey).list.map((itemData, i) => {
         if (itemData.type === 'task')
           return <Task
